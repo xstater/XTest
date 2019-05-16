84eb7494bcf0a26a3e290769a8024096f7cf8f86
xstater <a166224@163.com>
2019-05-17 00:38:35 +0800

    添加了对stack的测试

--- tests/test-benchmark.cpp
+++ tests/test-benchmark.cpp
@@ -5,13 +5,18 @@
 #include "Allocator.h"
 
 #include <vector>
+#include <stack>
 
 BENCHMARK_BEGIN(test1)
     auto &allocator = xtest::Allocator::instance();
     xtest::vector<int> vec;//use vector<T> in namespace xtest to track memory
     vec.push_back(12);
-    for(int i = 1; i <= 1000000;i++){
+    for(int i = 1; i <= 100;i++){
         vec.push_back(i);
+    }
+    xtest::stack<int> stk;
+    for(int i = 1; i <= 10000;++i){
+        stk.push(i);
     }
     int *p = (int*)allocator.allocate(sizeof(int)*100000);
     allocator.deallocate(p);
